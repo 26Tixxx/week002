fab302306781c65c2b324eba381aa4f30af0f317
26Tixxx <2572411962@qq.com>
2024-09-10 17:33:22 +0800

    Add files via upload

---
+++ week2 job dys/demo02.js
@@ -0,0 +1,14 @@
+let age = 25
+let Age = 30
+let name = 'dys'
+console.log(age)
+age = name
+console.log(age)
+// console.log(Age)
+
+
+alert('有分号');
+alert('没有分号')
+{
+    confirm('ok?')
+}
---
+++ week2 job dys/demo03.js
@@ -0,0 +1,7 @@
+let age =30;
+let name = "dys";
+let greeting ="I am "+name+". I am "+age+" years old.";
+console.log(greeting);
+let greeting2 = `I am ${name}. I am ${age} years old.`;
+console.log(greeting2)
+console.log(greeting2.length)
